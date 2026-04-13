bb0f8bb200bc4bc5495f88c919fe893ea984dc3b
suramaxi92 <suramaxi92@gmail.com>
2026-04-13 18:57:00 +0530

    Admin page modified

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import patients
 
 # Register your models here.
+class patientsAdmin(admin.ModelAdmin):
+    search_fields = ['name',
+                      ]
+    list_filter = ['last_visit',
+                   ]
+
+admin.site.register(patients, patientsAdmin)
